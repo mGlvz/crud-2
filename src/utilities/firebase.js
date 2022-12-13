9373d04d188979658fd5911a7c9dce5951b8acef
mGlvz <mogalvez@iu.edu>
2022-12-12 21:43:09 -0500

    commit

---
+++ src/utilities/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqHlYfdv7CsuwqLoSyqe_MENZPpyCJ7zA",
+    authDomain: "crud-2-b1dec.firebaseapp.com",
+    projectId: "crud-2-b1dec",
+    storageBucket: "crud-2-b1dec.appspot.com",
+    messagingSenderId: "400790441918",
+    appId: "1:400790441918:web:0dc680d6ae4aa8fe2e33ef"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
